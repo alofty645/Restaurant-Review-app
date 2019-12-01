435c013bf2a303918e791a8ef5b42b26bfdedcc5
Aden <aden.lofthouse@gmail.com>
2019-12-02 10:46:07 +1100

    updated styles

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@ var markers = [];
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener("DOMContentLoaded", event => {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker
+			.register("./sw.js")
+			.then(registration => {
+				// Registration was successful
+				console.log(
+					"ServiceWorker registered, scope: ",
+					registration.scope
+				);
+			})
+			.catch(error => {
+				// registration failed
+				console.log(
+					"ServiceWorker registration failed: ",
+					error
+				);
+			});
+	}
+
 	initMap(); // added
 	fetchNeighborhoods();
 	fetchCuisines();
@@ -92,19 +111,8 @@ initMap = () => {
 	).addTo(newMap);
 
 	updateRestaurants();
+	document.getElementById("map").tabIndex = "-1";
 };
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
 
 /**
  * Update page and map for current restaurants.
@@ -144,9 +152,7 @@ resetRestaurants = restaurants => {
 	ul.innerHTML = "";
 
 	// Remove all map markers
-	if (self.markers) {
-		self.markers.forEach(marker => marker.remove());
-	}
+	self.markers.forEach(m => m.setMap(null));
 	self.markers = [];
 	self.restaurants = restaurants;
 };
@@ -168,29 +174,66 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = restaurant => {
 	const li = document.createElement("li");
 
+	const link = document.createElement("a");
+	// link.innerHTML = 'View Details';
+	link.href = DBHelper.urlForRestaurant(restaurant);
+	link.className = restaurant.cuisine_type.toLowerCase();
+	link.setAttribute(
+		"aria-label",
+		"Details of " +
+			restaurant.name +
+			" restaurant, " +
+			restaurant.neighborhood
+	);
+	link.tabIndex = "0";
+	li.append(link);
+
 	const image = document.createElement("img");
 	image.className = "restaurant-img";
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
-	li.append(image);
+	image.alt = "Image of " + restaurant.name + " restaurant";
+	link.append(image);
+
+	const label = document.createElement("div");
+	label.className = "restaurant-label";
+	link.append(label);
 
 	const name = document.createElement("h1");
+	name.className = "restaurant-name";
 	name.innerHTML = restaurant.name;
-	li.append(name);
+	label.append(name);
 
 	const neighborhood = document.createElement("p");
 	neighborhood.innerHTML = restaurant.neighborhood;
-	li.append(neighborhood);
+	label.append(neighborhood);
 
 	const address = document.createElement("p");
+	address.className = "restaurant-address";
 	address.innerHTML = restaurant.address;
-	li.append(address);
+	label.append(address);
 
-	const more = document.createElement("a");
-	more.innerHTML = "View Details";
-	more.href = DBHelper.urlForRestaurant(restaurant);
-	li.append(more);
+	const hr = document.createElement("hr");
+	label.append(hr);
+
+	const rating = document.createElement("span");
+	rating.className = "rating";
+	rating.innerHTML = "Rating: " + restaurantRating(restaurant); //'rating';
+	label.append(rating);
+
+	const type = document.createElement("span");
+	type.className = "cuisine-type";
+	type.innerHTML = restaurant.cuisine_type;
+	label.append(type);
 
 	return li;
+};
+
+restaurantRating = restaurant => {
+	let reviews = restaurant.reviews.map(r => r.rating);
+	let rating = reviews.reduce((a, b) => a + b, 0) / reviews.length;
+	rating = rating.toFixed(1);
+
+	return rating;
 };
 
 /**
@@ -207,16 +250,5 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 		function onClick() {
 			window.location.href = marker.options.url;
 		}
-		self.markers.push(marker);
 	});
 };
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
